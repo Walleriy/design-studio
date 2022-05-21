81fa8dd78de2b5877b6a688a62b8336cfb6e943d
Valerii Stepanenko <vstepane@softserveinc.com>
2022-05-21 18:22:00 +0300

    Feature - update navbar

--- components/Navbar.Component.js
+++ components/Navbar.Component.js
@@ -2,13 +2,38 @@ import styles from '../styles/components/Navbar.module.scss'
 import Link from "next/link";
 
 export default function Navbar() {
+  const pages = [
+    {
+      url: "/",
+      name: "головна",
+    },
+    {
+      url: "/about",
+      name: "про нас",
+    },
+    {
+      url: "/services",
+      name: "послуги",
+    },
+    {
+      url: "/portfolio",
+      name: "портфоліо",
+    },
+    {
+      url: "/contacts",
+      name: "контакти",
+    },
+  ]
+
   return (
     <div className={styles.navbar}>
       <h1>куб</h1>
       <nav>
-        <Link href="/expindex">
-          <a className={styles.navbar_item}>expindex</a>
-        </Link>
+        {pages.map(({url, name}, key) => (
+          <Link href={url} key={key}>
+            <a className={styles.navbar_item}>{name}</a>
+          </Link>
+        ))}
       </nav>
     </div>
   )
