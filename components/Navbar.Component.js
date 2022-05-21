8369531252c06161308d9a03de489c317a932990
Valerii Stepanenko <vstepane@softserveinc.com>
2022-05-21 16:59:00 +0300

    Feature - add services block to main page

--- components/Navbar.Component.js
+++ components/Navbar.Component.js
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <Link href="/expindex">
-        <a className={styles.navbar_item}>expindex</a>
-      </Link>
-    </nav>
+    <div className={styles.navbar}>
+      <h1>куб</h1>
+      <nav>
+        <Link href="/expindex">
+          <a className={styles.navbar_item}>expindex</a>
+        </Link>
+      </nav>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
-import Image from 'next/image'
 import styles from '../styles/Main.module.scss'
-import photo from "../media/images/pexels-saya-kimura-401107 1.png"
+import Link from "next/link";
 
 export default function Main() {
+  const services = [
+    {
+      header: "ЛОГОТИПИ",
+      content: "Створення логотипу — один з перших, але найважливіших кроків у організації та розвитку бізнес-проекту. Для нас логотип це інструмент візуальної комунікації бренду і споживача, а не художнє самовираження дизайнера.",
+    },
+    {
+      header: "банери",
+      content: "Банери – це невід’ємний елемент реклами. Це означає, що банерна реклама одна з найпопулярніших та дієвіших методів розповісти про свій товар, послугу чи підприємство. Ми розуміємо, як зробити банер, який продаватиме ваш товар.\n",
+    },
+    {
+      header: "пакування",
+      content: "Привабливий і якісний дизайн упаковки - важливий атрибут бренду. Це не просто тара для продукту, а канал комунікації з аудиторією і найсильніший маркетинговий інструмент. Ми знаємо як зробити Ваше пакування винятковим!",
+    },
+  ]
+
+  const service = ({
+    header,
+    content,
+  }) => {
+    return (
+      <div className={styles.service}>
+        <h2>{header}</h2>
+        <div className={styles.content}>{content}</div>
+        <Link href="/services">
+          <a>
+            <img src="/images/arrow-right.svg"/>
+          </a>
+        </Link>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.main}>
-      <Image src={photo} alt='saya kimura' />;
+      <img src="/images/pexels-saya-kimura-401107.png" alt="saya kimura"/>
+      <div className={styles.services}>
+        {services.map(serviceItem => service(serviceItem))}
+      </div>
     </div>
   )
 }
