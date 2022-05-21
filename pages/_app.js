37dab97f22584a1ef746bb2668a35c6c8985dd8d
Valerii Stepanenko <vstepane@softserveinc.com>
2022-05-21 19:24:15 +0300

    Feature - add email sending

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import Navbar from "../components/Navbar.Component";
+import Contacts from "../components/Contacts.Component";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Navbar />
       <Component {...pageProps} />
+      <Contacts />
     </div>
   )
 }
